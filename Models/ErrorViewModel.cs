8f8fb1600f5d18a1d6c943e45feb8b43191f6b42
dhq652001 <danghoangquan06052001@gmail.com>
2022-10-01 00:26:36 +0700

    initial app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BaitaplonNet2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
